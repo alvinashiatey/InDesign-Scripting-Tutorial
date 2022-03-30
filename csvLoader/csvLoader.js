459fb398b1597488adb482771d36b2c466f22edd
alvinashiatey <alvinashiatey@gmail.com>
2022-03-30 12:43:01 -0400

    Update to csv loader to take in account the existing pages and loop through them before making new pages

--- csvLoader/csvLoader.js
+++ csvLoader/csvLoader.js
@@ -38,29 +38,30 @@ Array.prototype.reduce = function (callbackfn, initialValue) {
 Array.prototype.join = function (separator) {
   return this.reduce(function (acc, value) {
     return acc + value + separator;
-  }, "");
+  }, '');
 };
 String.prototype.trim = function () {
-  return this.replace(/^\s+|\s+$/g, "");
+  return this.replace(/^\s+|\s+$/g, '');
 };
 function main() {
-  var f = File.openDialog("Kindly select your CSV file", false),
-    docsData = [],
-    delimiter = ",";
-  if (!f) return;
+  var f = File.openDialog('Kindly select your CSV file', false), docsData = [], delimiter = ',';
+  if (!f)
+    return;
   if (!/\.csv$/i.test(f.name)) {
-    alert("Please select a CSV file");
+    alert('Please select a CSV file');
     return;
   }
-  f.open("r");
+  f.open('r');
   var text = f.read();
   docsData = convert(text, delimiter);
-  setupDoc();
+  var pages = setupDoc().pages;
   var _loop_1 = function (index, item) {
-    var pg = index === 0 ? addPage(false) : addPage();
+    var pg = pages ? addPage(pages) : addPage();
+    pages <= 0 ? (pages = 0) : pages--;
     var moveBottom = 0;
     Object.keys(item).forEach(function (key) {
-      if (item[key] === "" || item[key] === " ") return;
+      if (item[key] === '' || item[key] === ' ')
+        return;
       var dim = [pg.dim.TOP + moveBottom, pg.dim.LEFT, 40, pg.dim.RIGHT];
       var _t = addText(pg.page, dim, item[key]);
       moveBottom +=
@@ -70,16 +71,13 @@ function main() {
     moveBottom = 0;
   };
   for (var _i = 0, _a = docsData.entries(); _i < _a.length; _i++) {
-    var _b = _a[_i],
-      index = _b[0],
-      item = _b[1];
+    var _b = _a[_i], index = _b[0], item = _b[1];
     _loop_1(index, item);
   }
 }
 function setupDoc() {
   try {
-    var doc =
-      app.documents.length > 0 ? app.activeDocument : app.documents.add();
+    var doc = app.documents.length > 0 ? app.activeDocument : app.documents.add();
     // needed when working with spreads.
     doc.viewPreferences.rulerOrigin = RulerOrigin.PAGE_ORIGIN;
     app.activeDocument.viewPreferences.horizontalMeasurementUnits =
@@ -89,29 +87,30 @@ function setupDoc() {
     doc.textPreferences.smartTextReflow = true;
     doc.textPreferences.limitToMasterTextFrames = true;
     doc.textPreferences.addPages = AddPageOptions.END_OF_STORY;
-    return doc;
-  } catch (e) {
+    return { doc: doc, pages: doc.pages.length };
+  }
+  catch (e) {
     alert(e);
   }
 }
-function addPage(newPage) {
-  if (newPage === void 0) {
-    newPage = true;
-  }
+function addPage(pagesLength) {
+  if (pagesLength === void 0) { pagesLength = 0; }
   if (app.activeDocument) {
     var doc = app.activeDocument;
-    var page = newPage ? doc.pages.add() : doc.pages[0];
+    var page = pagesLength <= 0 ? doc.pages.add() : doc.pages[pagesLength - 1];
     var dim = {
       WIDTH: parseFloat(doc.documentPreferences.pageWidth.toString()),
       HEIGHT: parseFloat(doc.documentPreferences.pageHeight.toString()),
-      TOP: parseFloat(doc.marginPreferences.top.toString()),
+      TOP: parseFloat(doc.marginPreferences.top.toString())
     };
     dim.BOTTOM =
       dim.HEIGHT - parseFloat(doc.marginPreferences.bottom.toString());
     if (page.side === PageSideOptions.LEFT_HAND) {
       dim.LEFT = parseFloat(doc.marginPreferences.right.toString());
-      dim.RIGHT = dim.WIDTH - parseFloat(doc.marginPreferences.left.toString());
-    } else {
+      dim.RIGHT =
+        dim.WIDTH - parseFloat(doc.marginPreferences.left.toString());
+    }
+    else {
       dim.LEFT = parseFloat(doc.marginPreferences.left.toString());
       dim.RIGHT =
         dim.WIDTH - parseFloat(doc.marginPreferences.right.toString());
@@ -123,78 +122,80 @@ function addText(page, position, text) {
   if (app.activeDocument) {
     var doc = app.activeDocument;
     var layer = void 0;
-    if (doc.layers.item("CSV") == null) {
-      layer = doc.layers.add({ name: "CSV" });
-    } else {
-      layer = doc.layers.item("CSV");
+    if (doc.layers.item('CSV') == null) {
+      layer = doc.layers.add({ name: 'CSV' });
+    }
+    else {
+      layer = doc.layers.item('CSV');
     }
     var _tf = page.textFrames.add(layer);
     _tf.contents = text;
     _tf.visibleBounds = position;
     _tf.textFramePreferences.autoSizingReferencePoint =
       AutoSizingReferenceEnum.TOP_LEFT_POINT;
-    _tf.textFramePreferences.autoSizingType = AutoSizingTypeEnum.HEIGHT_ONLY;
+    _tf.textFramePreferences.autoSizingType =
+      AutoSizingTypeEnum.HEIGHT_ONLY;
     return _tf;
   }
 }
 function detectSeparator(csvFile) {
-  var separators = [",", ";", "\t"];
-  var counts = {},
-    sepMax;
+  var separators = [',', ';', '\t'];
+  var counts = {}, sepMax;
   for (var _i = 0, separators_1 = separators; _i < separators_1.length; _i++) {
     var sep = separators_1[_i];
-    var re = new RegExp(sep, "g");
+    var re = new RegExp(sep, 'g');
     counts[sep] = (csvFile.match(re) || []).length;
     sepMax = !sepMax || counts[sep] > counts[sepMax] ? sep : sepMax;
   }
   return sepMax;
 }
 function csvToArray(text) {
-  var p = "",
-    row = [""],
-    ret = [row],
-    i = 0,
-    r = 0,
-    s = !0,
-    l;
+  var p = '', row = [''], ret = [row], i = 0, r = 0, s = !0, l;
   for (var _i = 0, text_1 = text; _i < text_1.length; _i++) {
     l = text_1[_i];
     if ('"' === l) {
-      if (s && l === p) row[i] += l;
+      if (s && l === p)
+        row[i] += l;
       s = !s;
-    } else if ("," === l && s) l = row[++i] = "";
-    else if ("\n" === l && s) {
-      if ("\r" === p) row[i] = row[i].slice(0, -1);
-      row = ret[++r] = [(l = "")];
+    }
+    else if (',' === l && s)
+      l = row[++i] = '';
+    else if ('\n' === l && s) {
+      if ('\r' === p)
+        row[i] = row[i].slice(0, -1);
+      row = ret[++r] = [(l = '')];
       i = 0;
-    } else row[i] += l;
+    }
+    else
+      row[i] += l;
     p = l;
   }
   return ret;
 }
 var csvParser = function (strData, header) {
-  if (header === void 0) {
-    header = true;
-  }
-  var headers = strData.split("\n")[0].split(",");
-  var data = strData.slice(strData.indexOf("\n") + 1);
+  if (header === void 0) { header = true; }
+  var headers = strData.split('\n')[0].split(',');
+  var data = strData.slice(strData.indexOf('\n') + 1);
   var arrData = csvToArray(data);
   if (header) {
     return arrData.map(function (row) {
       var i = 0;
       return headers.reduce(function (acc, key) {
-        acc[key] = (row[i++] || "").trim().replace(/(^")|("$)/g, "");
+        acc[key] = (row[i++] || '').trim().replace(/(^")|("$)/g, '');
         return acc;
       }, {});
     });
-  } else {
+  }
+  else {
     return arrData;
   }
 };
 var convert = function (csvData, delimiter) {
-  if (csvData.length == 0) throw Error("CSV data is empty");
+  if (csvData.length == 0)
+    throw Error('CSV data is empty');
   var separator = delimiter || detectSeparator(csvData);
-  if (!separator) throw Error("Separator not found");
+  if (!separator)
+    throw Error('Separator not found');
   var a = [];
   a = csvParser(csvData, true);
   return a;
